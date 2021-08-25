9b1adecaa124e9d7d4c45516ac21e18fff90c011
Anael Bouchema <anael.bouchema@korys.io>
2021-08-25 11:23:18 +0200

    Changement du ciblage des boutons pour le onClick côté front

--- frontend/src/RelayButtons.tsx
+++ frontend/src/RelayButtons.tsx
@@ -18,17 +18,17 @@ export default function RelayButtons() {
     const classes = useStyles();
 
     const getRelay = (e: any): void => {
-        axios.post(URL_API_RELAY + e.target.offsetParent.accessKey)
+        axios.post(URL_API_RELAY + e.target.offsetParent.title)
             .catch(err => console.error("Post request ==> " + err));
         console.log(e)
     }
 
     return (
         <div className={classes.root}>
-            <Button variant="contained" color="primary" accessKey="relayOne" onClick={getRelay}> Relay 1 </Button>
-            <Button variant="contained" color="primary" accessKey="relayTwo" onClick={getRelay}> Relay 2</Button>
-            <Button variant="contained" color="primary" accessKey="relayThree" onClick={getRelay}> Relay 3</Button>
-            <Button variant="contained" color="primary" accessKey="relayFour" onClick={getRelay}> Relay 4</Button>
+            <Button variant="contained" color="primary" title="relayOne" onClick={getRelay}> Relay 1 </Button>
+            <Button variant="contained" color="primary" title="relayTwo" onClick={getRelay}> Relay 2</Button>
+            <Button variant="contained" color="primary" title="relayThree" onClick={getRelay}> Relay 3</Button>
+            <Button variant="contained" color="primary" title="relayFour" onClick={getRelay}> Relay 4</Button>
         </div>
     );
 }
