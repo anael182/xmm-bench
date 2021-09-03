1e8afb9c38461f4f876ab63858b8f881e925c4c2
Anael Bouchema <anael.bouchema@korys.io>
2021-09-03 17:07:05 +0200

    Frontend => Login.tsx : SetInterval deleted on Login.tsx component, added async fetching function.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,24 @@
 import Webcam from "./Webcam";
 import RelayButtons from './RelayButtons';
 import Login from "./Login";
+/*
 import {Grid} from "@material-ui/core";
+*/
 
 
 function App() {
     return (
-        <Grid container justifyContent = "center">
-        <div className="App">
+
+        /*<Grid container justifyContent = "center">*/
+
+
+        <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}} className="App">
             <Login />
             <Webcam/>
             <RelayButtons/>
         </div>
-        </Grid>
-
     );
+        {/*</Grid>*/}
 }
 
 export default App;
--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -4,26 +4,25 @@ import ReleaseToken from "./ReleaseToken";
 import axios from "axios";
 import {Alert} from "@material-ui/lab";
 
-
 export default function Login() {
 
     const [userName, setUserName] = useState("");
     const [date, setDate] = useState("");
 
-    useEffect(() => {
-        setInterval(() => {
-            axios.get(process.env.React_App_URL_API + "reservation/state")
-                .then((res)=> {
-                    if (res.data === null) {
-                        setUserName("");
-                    }else{
-                        setUserName(res.data.username);
-                        setDate(res.data.token_creation_date);
-                    }
-                })
-                .catch(err => console.error(err));
-        }, 5000);
-    });
+
+    useEffect( () => {
+        const fetchData = async () => {
+            const result = await axios(process.env.React_App_URL_API + "reservation/state");
+            if (result.data === null) {
+                setUserName("");
+            } else {
+                setUserName(result.data.username);
+                setDate(result.data.token_creation_date);
+                console.log(result);
+            }
+        };
+        fetchData();
+    },[]);
 
     return (
         <div>
