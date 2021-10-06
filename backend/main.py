7f609cd5ea2bd913f4769d8e987a4669d638dd9f
Alexandre Chataignon <alexandre.chataignon@korys.io>
2021-10-06 18:39:05 +0200

    Cleanup code backend

--- backend/main.py
+++ backend/main.py
@@ -43,6 +43,10 @@ class Token(BaseModel):
     expires_date: Optional[datetime]
 
 
+class QueueResponse(BaseModel):
+    queue: list[InputToken]
+
+
 #
 # Webhook management
 #
@@ -54,16 +58,13 @@ def teams_webhook(summary: str, activity: str, name_message: str, value_message:
         "@context": "http://schema.org/extensions",
         "themeColor": "0076D7",
         "summary": summary,
-        "sections": [{
-            "activityTitle": activity,
-            "facts": [
-                {
-                    "name": name_message,
-                    "value": value_message
-                }
-            ],
-            "markdown": True
-        }]
+        "sections": [
+            {
+                "activityTitle": activity,
+                "facts": [{"name": name_message, "value": value_message}],
+                "markdown": True,
+            }
+        ],
     }
     if os.getenv("WEBHOOK_URL") is not None:
         requests.post(url=os.getenv("WEBHOOK_URL"), json=data)
@@ -76,29 +77,46 @@ def teams_webhook(summary: str, activity: str, name_message: str, value_message:
 token = None
 
 
-async def check_token_expiration():
+def take_token(input_token):
+    global token
+    token = Token(
+        creation_date=datetime.now(),
+        expires_date=datetime.now() + timedelta(minutes=input_token.token_minutes)
+        if input_token.token_minutes
+        else None,
+        username=input_token.username,
+    )
+    teams_webhook(
+        f'{token.username} is using {os.getenv("BOARD_NAME")}',
+        f'{token.username} is using {os.getenv("BOARD_NAME")}'
+        f' since {token.creation_date.strftime("%d/%m/%Y %H:%M:%S")}',
+        "Token claimed until" if token.expires_date else "No expiration time",
+        f'{token.expires_date.strftime("%d/%m/%Y %H:%M:%S")}'
+        if token.expires_date
+        else "",
+    )
+
+
+async def check_token_expiration(force_release=False):
     global token
     global queue
-    if token and datetime.now() >= token.expires_date:
-        teams_webhook(f'{token.username} just released {os.getenv("BOARD_NAME")}',
-                      f'{token.username} just released {os.getenv("BOARD_NAME")}',
-                      'Token duration expired', "")
+    if token is None:
+        return
+
+    if force_release or datetime.now() >= token.expires_date:
+        teams_webhook(
+            f'{token.username} just released {os.getenv("BOARD_NAME")}',
+            f'{token.username} just released {os.getenv("BOARD_NAME")}',
+            "Manual release of the token"
+            if force_release
+            else "Token duration expired",
+            "",
+        )
+        token = None
+
         if len(queue) >= 1:
-            token = Token(
-                creation_date=datetime.now(),
-                expires_date=datetime.now() + timedelta(minutes=queue[0].token_minutes) if queue[
-                    0].token_minutes else None,
-                username=queue[0].username,
-            )
-            teams_webhook(f'{token.username}  is using {os.getenv("BOARD_NAME")}',
-                          f'{token.username} is using {os.getenv("BOARD_NAME")} since {token.creation_date.strftime("%d/%m/%Y %H:%M:%S")}',
-                          'token claimed until', f'{token.expires_date.strftime("%d/%m/%Y %H:%M:%S")}')
-            queue.popleft()
-        elif len(queue) == 0:
-            teams_webhook(f'{token.username} just released {os.getenv("BOARD_NAME")}',
-                          f'{token.username} just released {os.getenv("BOARD_NAME")}',
-                          'The board is free', "")
-            token = None
+            token_input = queue.popleft()
+            take_token(token_input)
 
 
 async def background_task():
@@ -106,7 +124,7 @@ async def background_task():
         try:
             await asyncio.sleep(1)
             await check_token_expiration()
-        except:
+        except Exception:
             print("Oops! Something went wrong.")
 
 
@@ -117,6 +135,7 @@ async def background_task():
 
 async def async_stream(stream):
     import asyncio
+
     cap = stream.video_capture
     fd = cap.device.fileno()
     loop = asyncio.get_event_loop()
@@ -134,42 +153,42 @@ async def async_stream(stream):
 
 
 class WebcamRunner:
-
     def __init__(self):
         try:
             self.cam = v4l2py.Device.from_id(0)
-            self.cam.video_capture.set_format(800, 600, 'MJPG')
-        except:
+            self.cam.video_capture.set_format(800, 600, "MJPG")
+        except Exception:
             self.cam = None
 
         self.current = None
 
     async def run_capture(self):
         if self.cam:
-            async for frame in async_stream(v4l2py.device.VideoStream(self.cam.video_capture)):
+            async for frame in async_stream(
+                v4l2py.device.VideoStream(self.cam.video_capture)
+            ):
                 self.current = frame
 
     async def video_streamer(self, fps: int):
         if self.cam:
             while True:
-                yield b"--jpgboundary\r\n" + b'Content-Type: image/jpeg\r\n\r\n' + self.current
+                yield (
+                    b"--jpgboundary\r\n"
+                    + b"Content-Type: image/jpeg\r\n\r\n"
+                    + self.current
+                )
                 await asyncio.sleep(1 / fps)
         else:
-            yield b"--jpgboundary\r\n" + b'Content-Type: image/jpeg\r\n\r\n' + open('images/image.jpeg', 'rb').read()
+            yield b"--jpgboundary\r\n" + b"Content-Type: image/jpeg\r\n\r\n" + open(
+                "images/image.jpeg", "rb"
+            ).read()
 
 
 webcam_runner = WebcamRunner()
 
 
-@app.on_event('startup')
-async def app_startup():
-    asyncio.create_task(webcam_runner.run_capture())
-    asyncio.create_task(background_task())
-
-
 @app.get("/webcam/{fps}")
-# http://127.0.0.1:8000/webcam/
-def webcam(fps: int):
+async def webcam(fps: int):
     return StreamingResponse(
         webcam_runner.video_streamer(fps),
         media_type="multipart/x-mixed-replace; boundary=--jpgboundary",
@@ -185,21 +204,19 @@ XMM_RELAY_BIN = os.getenv("XMM_RELAY_BIN", "/home/korys/bin/xmm_relay")
 
 
 @app.post("/relay/{name}")
-# http://127.0.0.1:8000/relay/
-def toggle_relay(name: str):
-    if name == 'flash':
+async def toggle_relay(name: str):
+    if name == "flash":
         r = subprocess.run([XMM_RELAY_BIN, "-f"])
-    elif name == 'power':
+    elif name == "power":
         r = subprocess.run([XMM_RELAY_BIN, "-p"])
-    elif name == 'reboot':
+    elif name == "reboot":
         r = subprocess.run([XMM_RELAY_BIN, "-r"])
-    elif name == 'cv22boot':
+    elif name == "cv22boot":
         r = subprocess.run([XMM_RELAY_BIN, "-c"])
-    elif name == 'cv22flash':
+    elif name == "cv22flash":
         r = subprocess.run([XMM_RELAY_BIN, "-v"])
 
-    r = StatusResponse(message="", status=r.returncode == 0)
-    return r
+    return StatusResponse(message="", status=r.returncode == 0)
 
 
 #
@@ -210,117 +227,78 @@ queue = deque()
 
 
 @app.post("/reservation/take")
-# http://127.0.0.1:8000/reservation/take
-# JSON INPUT EXAMPLE {"username" : "Johndoe"}
-def create_access_token(input_token: InputToken, response: Response):
+async def create_access_token(input_token: InputToken, response: Response):
     global token
-    if token is None and input_token.token_minutes is not None:
-        token = Token(
-            creation_date=datetime.now(),
-            expires_date=datetime.now() + timedelta(minutes=input_token.token_minutes),
-            username=input_token.username,
-        )
-        print("Creation du token : " + str(token))
-        teams_webhook(f'{token.username}  is using {os.getenv("BOARD_NAME")}',
-                      f'{token.username} is using {os.getenv("BOARD_NAME")} since {token.creation_date.strftime("%d/%m/%Y %H:%M:%S")}',
-                      'token claimed until', f'{token.expires_date.strftime("%d/%m/%Y %H:%M:%S")}')
-        return {
-            'creation_date': token.creation_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'expires_date': token.expires_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'username': token.username,
-        }
-    elif token is None and input_token.token_minutes is None:
-        token = Token(
-            creation_date=datetime.now(),
-            expires_date=None,
-            username=input_token.username,
-        )
-        teams_webhook(f'{token.username}  is using {os.getenv("BOARD_NAME")}',
-                      f'{token.username} is using {os.getenv("BOARD_NAME")} since {token.creation_date.strftime("%d/%m/%Y %H:%M:%S")}',
-                      "No expiration time", "")
-        return {
-            'creation_date': token.creation_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'expires_date': None,
-            'username': token.username,
-        }
+    if token is None:
+        take_token(input_token)
+        return token
     else:
         response.status_code = status.HTTP_409_CONFLICT
-        r = StatusResponse(
+        return StatusResponse(
             message="Token is already taken by " + token.username, status=False
         )
-        return r
 
 
 @app.post("/reservation/release")
-# http://127.0.0.1:8000/reservation/release
-def release_token(response: Response):
+async def release_token(response: Response):
     global token
     global queue
     if token is None:
         response.status_code = status.HTTP_400_BAD_REQUEST
-        r = StatusResponse(message="Token is already free", status=False)
-        return r
+        return StatusResponse(message="Token is already free", status=False)
     else:
         print("Last user => " + token.username)
-        token.expires_date = datetime.now()
-        check_token_expiration()
-    r = StatusResponse(message="Token released", status=True)
-    return r
+        await check_token_expiration(force_release=True)
+        return StatusResponse(message="Token released", status=True)
 
 
 @app.get("/reservation/state")
-# http://127.0.0.1:8000/reservation/state
 async def token_state():
     global token
-    if token is not None and token.expires_date is not None:
-        return {
-            'creation_date': token.creation_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'expires_date': token.expires_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'username': token.username,
-        }
-    elif token is not None and token.expires_date is None:
-        return {
-            'creation_date': token.creation_date.strftime("%d/%m/%Y %H:%M:%S"),
-            'username': token.username,
-        }
-    else:
-        return None
+    return token
 
 
 @app.post("/reservation/queue/join")
-def queue_management_add(input_token: InputToken):
+async def queue_management_add(input_token: InputToken):
     global queue
     queue.append(input_token)
-    return {"input_token": input_token, "queue_position": len(queue)}
+    return StatusResponse(
+        status=True, message=f"Joined queue at position {len(queue)}:"
+    )
 
 
 @app.post("/reservation/queue/leave/{index}")
-def queue_management_delete(index: int):
+async def queue_management_delete(index: int):
     global queue
     if len(queue) >= 1:
         if index <= len(queue):
-            print(f"{queue[index].username} has left the queue.")
-            teams_webhook(f"{queue[index].username.capitalize()} has left the queue.",
-                          f"{queue[index].username.capitalize()} has left the queue.", "", "")
             del queue[index]
-            return {"queue": list(queue)}
+            return QueueResponse(queue=list(queue))
         else:
-            r = StatusResponse(message="Index out of bounds", status=True)
-            return r
+            return StatusResponse(message="Index out of bounds", status=False)
     else:
-        r = StatusResponse(message="The queue is empty", status=True)
-        return r
+        return StatusResponse(message="The queue is empty", status=False)
 
 
 @app.get("/reservation/queue/state")
-def get_queue():
-    return {"queue": list(queue)}
+async def get_queue():
+    return QueueResponse(queue=list(queue))
 
 
 @app.get("/board")
-# http://127.0.0.1:8000/board
-def get_board():
+async def get_board():
     if os.getenv("BOARD_NAME") is not None:
         return {"board_name": os.getenv("BOARD_NAME")}
     else:
         return {"board_name": None}
+
+
+#
+# Startup
+#
+
+
+@app.on_event("startup")
+async def app_startup():
+    asyncio.create_task(webcam_runner.run_capture())
+    asyncio.create_task(background_task())
